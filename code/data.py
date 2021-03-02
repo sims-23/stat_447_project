89441eaa3135b60f1e2a0b6e87579295f4a67c4f
sims-23 <sim.sang.computers@gmail.com>
2021-03-01 18:12:14 -0800

    Late upload. Data processing paritally. Plots still need to be made depending on what we decide. Sad life - git issue and my incompetence deleted previous version

---
+++ code/data.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import data_utils as du
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+train = pd.read_csv('data/train.csv', nrows=100000)
+
+text = train.isna().sum(axis=0)
+print(train.columns)
+du.save_table_fig(text, 'nas_cols')
+
+# determines how many rows contain the max num of nas (3)
+print(train[train.isna().sum(axis=1)==3].shape[0])
+
+# imputations for variables
+# reason is approximately 36% of data is nas - too high
+train.drop('orig_destination_distance', axis=1, inplace=True)
+
+du.dates_to_vars(train)
+
+print(train[['srch_ci', 'srch_co', 'stay_dur']])
+
+print(train.corr()['hotel_cluster'])
+
+# same magnitudes (i.e. abs vals) get same colour
+cmap = sns.diverging_palette(240, 240, as_cmap=True)
+
+corr_table = train.corr()
+cols_corr = corr_table.columns.size
+
+plt.figure()
+fig = sns.heatmap(train.corr()['hotel_cluster'].values.reshape(cols_corr,1),
+                  yticklabels=corr_table.columns,  annot=True, linewidths=2, cmap=cmap)
+plt.xlabel('hotel_cluster')
+du.save_fig('corr')
+
+
---
+++ code/data_utils.py
@@ -0,0 +1,44 @@
+import os.path
+import pandas as pd
+
+import numpy as np
+from numpy.linalg import norm
+import matplotlib.pyplot as plt
+from pandas.plotting import table
+import scipy.sparse
+from scipy.optimize import approx_fprime
+
+def save_fig(fname, verbose=True):
+    path = os.path.join('figs', fname)
+    plt.savefig(path, bbox_inches='tight')
+    if verbose:
+        print("Figure saved as '{}'".format(path))
+
+def save_table_fig(df, fname, verbose=True):
+    ax = plt.subplot(frame_on=False)
+    ax.xaxis.set_visible(False)
+    ax.yaxis.set_visible(False)
+    table(ax, df, loc='center')
+    save_fig(fname, verbose)
+
+def dates_to_vars(df):
+    df[['srch_ci', 'srch_co', 'date_time']] = df[['srch_ci', 'srch_co', 'date_time']].apply(pd.to_datetime)
+    df['stay_dur'] = (df['srch_co'] - df['srch_ci']).dt.days
+    df['no_days_to_checkin'] = (df['srch_ci'] - df['date_time']).dt.days
+    # should we do number of weeks to checkin, number of months, number of days?
+
+    # For hotel check-in
+    # day of month (1 to 31)
+    df['Cin_day'] = df["srch_ci"].dt.day
+    # 0 for Monday, 6 for Sunday
+    df['Cin_day_of_week'] = df["srch_ci"].dt.weekday
+    df['Cin_week'] = df["srch_ci"].dt.isocalendar().week
+    df['Cin_month'] = df["srch_ci"].dt.month
+    df['Cin_year'] = df["srch_ci"].dt.year
+#
+# def drop_vars(df):
+
+
+
+
+
